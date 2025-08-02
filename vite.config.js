252bcae4c082c0edd37b4710f9197ee5f469517c
Nil-lee <jimmerican55@gmail.com>
2025-08-02 21:34:22 +0800

    change html title

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/你的 GitHub 專案名/", // 這行很重要！不然頁面會是空白
+  base: "/react-todo-list/", // ← 這裡要跟你的 GitHub repo 名字完全一樣！
   plugins: [react()],
 });
